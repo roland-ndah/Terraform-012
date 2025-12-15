ace9ffdccab0dff8b3db4b34ef7a7da3a1d331d3
Roland Ndah <ndah.roland@yahoo.com>
2025-12-14 22:50:20 -0500

    terraform vpc via pipeline

--- main.tf
+++ main.tf
@@ -44,3 +44,29 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "roland-ec2-sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "roland-ec2-sg"
+  }
+}
+
