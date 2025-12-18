c73eae062f22bef49bf903e49dfa82e371afcd6f
Roland Ndah <ndah.roland@yahoo.com>
2025-12-18 13:41:03 -0500

    terraform vpc via pipeline

--- main.tf
+++ main.tf
@@ -16,14 +16,15 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "roland-public-subnet"
+    Name = "roland-public-subnet2"
   }
 }
 
